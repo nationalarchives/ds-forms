4fa5eb5579c63d95a03fc17ac59c6ccb7b3c2fde
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-02-23 21:17:52 +0000

    Implement unique ID generation and enhance email content processing in EmailResultHandler

--- app/forms/result_handlers.py
+++ app/forms/result_handlers.py
@@ -1,9 +1,9 @@
-import json
+import uuid
 from abc import ABC
 from functools import reduce
 
 import boto3
-from flask import current_app, render_template
+from flask import current_app, render_template, render_template_string
 from requests import codes, get, post
 
 
@@ -26,11 +26,17 @@ class ResultHandler(ABC):
             "Subclasses must implement the __init__ dunder method"
         )
 
+    def id(self) -> str:
+        return uuid.uuid4().hex
+
     def process(self, data: dict, **kwargs):
         raise NotImplementedError("Subclasses must implement the process method")
 
     def send(self, **kwargs) -> bool:
         raise NotImplementedError("Subclasses must implement the send method")
+
+    def result(self) -> dict:
+        raise NotImplementedError("Subclasses must implement the result method")
 
 
 class EmailResultHandler(ResultHandler):
@@ -52,23 +58,37 @@ class EmailResultHandler(ResultHandler):
             aws_session_token=aws_session_token,
             region_name=region_name,
         )
-        self.content: str = ""
         self.data: dict = {}
+        self.content: dict = {}
+        self.result_data: dict = {}
 
     def process(self, data: dict, **kwargs):
-        if template := kwargs.get("template"):
-            self.content = render_template(
-                template,
-                responses=data,
-            )
-        else:
-            self.content = json.dumps(data, indent=2)
+        print("----------- Processing email content with data:", data)
         self.data = data
+        self.data["reference_number"] = self.id()
+        print(self.data)
+        self.content = {
+            "template": kwargs.get("template") or "layouts/email.html",
+            "content": render_template_string(kwargs.get("content", ""), **self.data),
+            "cta_buttons": kwargs.get("cta_buttons", []),
+            "signoff": render_template_string(kwargs.get("signoff", ""), **self.data),
+        }
+        if "panel" in kwargs:
+            self.content["panel"] = {
+                "title": kwargs["panel"].get("title", ""),
+                "body": render_template_string(
+                    kwargs["panel"].get("body", ""), **self.data
+                ),
+            }
+
+        print(self.content)
 
     def send(self, **kwargs) -> bool:
+        print("==========================EMAIL CONTENT START==========================")
         current_app.logger.debug("Sending email")
-        if not self.content:
-            raise ValueError("Email content is empty. Call process() first.")
+        if not self.data or not self.content:
+            raise ValueError("Email not processed. Call process() with the data first.")
+        content = render_template(self.content["template"], **self.content)
         to_email = kwargs.get("to", "")
         if not to_email:
             if to_email_var := kwargs.get("toVar", ""):
@@ -83,14 +103,25 @@ class EmailResultHandler(ResultHandler):
                 Destination={"ToAddresses": [to_email]},
                 Message={
                     "Subject": {"Data": subject},
-                    "Body": {"Html": {"Charset": "UTF-8", "Data": self.content}},
+                    "Body": {"Html": {"Charset": "UTF-8", "Data": content}},
                 },
             )
-            current_app.logger.debug(f"Email sent with ID {response.get('MessageId')}")
+            id = response.get('MessageId')
+            current_app.logger.debug(f"Email sent with ID {id}")
+            self.result_data = {"id": id}
             return True
         except Exception as e:
             current_app.logger.error(f"EmailResultHandler error: {e}")
             return False
+        self.result_data = {"id": self.id()}
+        print(content)
+        print(self.content)
+        print(self.data)
+        # print(f"Email content: {content[20000:]}")
+        return True
+
+    def result(self) -> dict:
+        return self.result_data
 
 
 class APIResultHandler(ResultHandler):
@@ -133,6 +164,9 @@ class APIResultHandler(ResultHandler):
             current_app.logger.error(f"APIResultHandler error: {e}")
             return False
 
+    def result(self) -> dict:
+        return {}
+
 
 class PostgresResultHandler(ResultHandler):
     """
@@ -146,7 +180,10 @@ class PostgresResultHandler(ResultHandler):
         pass
 
     def send(self, **kwargs) -> bool:
-        pass
+        return False
+
+    def result(self) -> dict:
+        return {}
 
 
 class MongoDBResultHandler(ResultHandler):
@@ -161,4 +198,7 @@ class MongoDBResultHandler(ResultHandler):
         pass
 
     def send(self, **kwargs) -> bool:
-        pass
+        return False
+
+    def result(self) -> dict:
+        return {}
