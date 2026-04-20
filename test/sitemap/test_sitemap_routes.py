652c20198e800def93bb748b7db9ee52b251745c
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-04-20 16:59:43 +0100

    Add TNA Python utilities, remove Flask Talisman, update dependencies, fix log level, remove API client (#4)

--- test/sitemap/test_sitemap_routes.py
+++ test/sitemap/test_sitemap_routes.py
@@ -5,11 +5,12 @@ from app import create_app
 
 class SitemapBlueprintTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app("config.Test").test_client()
+        self.app = create_app("config.Test")
+        self.client = self.app.test_client()
         self.domain = "https://localhost"
 
     def test_sitemap(self):
-        rv = self.app.get("/forms/sitemap.xml")
+        rv = self.client.get("/forms/sitemap.xml")
         self.assertEqual(rv.status_code, 200)
         self.assertIn(
             f"<loc>{self.domain}/apply-to-film-at-the-national-archives/</loc>", rv.text
